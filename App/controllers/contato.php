b7e62c7d45cbb57f6a589feeef771d9c0f6289d3
ReginaldoAzevedo <reginaldo@raphoto.com.br>
2021-02-15 22:16:02 -0300

    Criação do README

---
+++ App/controllers/contato.php
@@ -0,0 +1,18 @@
+<?php
+
+class Contato {
+
+    public function index() {
+        echo"index do contato";
+    }
+    
+    public function email() {
+
+        echo"teste@teste.com";
+    }
+
+
+    public function telefone() {
+        echo"11 999937659";
+    }
+}
